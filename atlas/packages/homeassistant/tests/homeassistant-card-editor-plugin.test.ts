1ed61c72c7cbafc2f1eb3068b47339840b8c03c4
Uwe Grimm <rockbaer2007@live.de>
2026-09-09 19:56:03 +0200

    Update ATLAS add-on to 0.1.193

--- atlas/packages/homeassistant/tests/homeassistant-card-editor-plugin.test.ts
+++ atlas/packages/homeassistant/tests/homeassistant-card-editor-plugin.test.ts
@@ -24,7 +24,7 @@ describe("Home Assistant card editor plugin", () => {
     expect(plugin.manifest).toMatchObject({
       id: HomeAssistantCardEditorPluginId,
       name: "ATLAS Home Assistant Card Editor",
-      version: "0.2.0-alpha.81",
+      version: "0.2.0-alpha.82",
       extensionPoints: [
         HomeAssistantCardEditorExtensionPoints.cardEditor,
         HomeAssistantCardEditorExtensionPoints.cardTarget,
